fe48141435163ddf62243815b3043bf7cf558b6b
H2-T23 <steppingbird@yahoo.co.jp>
2013-10-30 17:15:07 +0900

    Create SessionState.h

---
+++ Session/SessionState.h
@@ -0,0 +1,8 @@
+// SessionState.h 
+#pragma once
+
+typedef enum EnumSessionState {
+	SESSION_RECEIVE_PROCESSING	= 1	,
+	SESSION_REQUEST_PROCESSING		,
+	SESSION_RESULT_PROCESSING		,
+} ESessionState;
