5b0ffc7134a8907b70b68f4c73279b641353a5b4
H2-T23 <steppingbird@yahoo.co.jp>
2013-12-26 17:57:56 +0900

    Update Panel.h

--- GUIToolKit/src/Panel.h
+++ GUIToolKit/src/Panel.h
@@ -15,8 +15,12 @@ protected:
 	std::list<CWnd*>		listWnd;
 
 public:
-	BOOL		Create( CWnd* pParent, CRect& rect ){
-		return TWnd<CPanel>::Create(NULL,0,0,rect.top,rect.left,rect.Width(),rect.Height(),(HWND)(*pParent),0);
+	BOOL		Create( CWnd* pParent, int x, int y, int nWidth, int nHeight, LPCTSTR lpWindowName = NULL ){
+		return TWnd<CPanel>::Create( lpWindowName, 0, 0, x, y, nWidth, nHeight, (HWND)(*pParent) );
+	}
+
+	BOOL		Create( CWnd* pParent, CRect& rect, LPCTSTR lpWindowName = NULL ){
+		return CPanel::Create( pParent, rect.X(), rect.Y(), rect.Width(), rect.Height(), lpWindowName );
 	}
 
 	void		Append( CWnd* pChild ){
