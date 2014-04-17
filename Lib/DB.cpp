207bf93e2bb2345441d029a0a7d776940b6ed401
H2-T23 <steppingbird@yahoo.co.jp>
2014-04-18 07:13:05 +0900

    TableData

--- Lib/DB.cpp
+++
@@ -1,8 +0,0 @@
-// DataBase.cpp 
-#include "DB.h"
-
-int main(int argc,char** argv)
-{
-	DB::UnitTest();
-	return 0;
-}
--- Lib/Variant.cpp
+++
@@ -1,8 +0,0 @@
-// Variant.cpp 
-#include "Variant.h"
-
-int main(int argc,char** argv)
-{
-	VARIANT::UnitTest();
-	return 0;
-}
