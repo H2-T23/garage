33f80747065695393e5b329459775c6574e8874e
H2-T23 <steppingbird@yahoo.co.jp>
2014-05-20 17:18:38 +0900

    Update FileOpenDlg.h

--- Toolkit/FileOpenDlg.h
+++ Toolkit/FileOpenDlg.h
@@ -14,8 +14,8 @@ protected:
 	TCHAR			m_szFileName[ MAX_PATH ];
 
 	virtual LPTSTR			Title( void ) const		{ return _T("File Open");	}
-	virtual LPCTSTR			Filter( void ) const	{ return _T("All files(*.*)\0*.*\0\0");	}
-	virtual LPCTSTR			DefExt( void ) const	{ return _T(""); }
+	virtual LPCTSTR			Filter( void ) const	{ return _T("Text(*.txt)\0*.txt\0All files(*.*)\0*.*\0\0");	}
+	virtual LPCTSTR			DefExt( void ) const	{ return _T("txt"); }
 	virtual	DWORD			Flags( void ) const		{ return(OFN_FILEMUSTEXIST);	}
 
 public:
@@ -45,16 +45,6 @@ public:
  *
  *
  */
-class CTextOpenDlg : public CFileOpenDlg {
-protected:
-	virtual LPCTSTR			Filter( void ) const	{ return _T("Text(*.txt)\0*.txt\0All files(*.*)\0*.*\0\0");	}
-	virtual LPCTSTR			DefExt( void ) const	{ return _T("obj"); }
-};
-/**********************************************************************************
- *
- *
- *
- */
 class CObjOpenDlg : public CFileOpenDlg {
 protected:
 	virtual LPCTSTR			Filter( void ) const	{ return _T("Object(*.obj)\0*.obj\0All files(*.*)\0*.*\0\0");	}
@@ -69,40 +59,4 @@ class CExeOpenDlg : public CFileOpenDlg {
 protected:
 	virtual LPCTSTR			Filter( void ) const	{ return _T("Execute(*.exe)\0*.exe\0All files(*.*)\0*.*\0\0");	}
 	virtual LPCTSTR			DefExt( void ) const	{ return _T("exe"); }
-};
-/**********************************************************************************
- *
- *
- *
- */
-class CFileSaveDlg {
-protected:
-	TCHAR			m_szFileName[ MAX_PATH ];
-
-	virtual LPTSTR			Title( void ) const		{ return _T("File Open");	}
-	virtual LPCTSTR			Filter( void ) const	{ return _T("All files(*.*)\0*.*\0\0");	}
-	virtual LPCTSTR			DefExt( void ) const	{ return _T(""); }
-	virtual	DWORD			Flags( void ) const		{ return(OFN_OVERWRITEPROMPT);	}
-
-public:
-	BOOL	DoModal( HWND hWnd ){
-		ZeroMemory(m_szFileName, sizeof(m_szFileName));
-
-		OPENFILENAME		ofn;
-		ZeroMemory(&ofn, sizeof(ofn));
-		ofn.lStructSize		= sizeof(OPENFILENAME);
-		ofn.hwndOwner		= hWnd;
-		ofn.lpstrFilter		= Filter();
-		ofn.lpstrFile		= m_szFileName;
-		ofn.nMaxFile		= MAX_PATH;
-		ofn.Flags			= Flags();
-		ofn.lpstrFileTitle	= Title();
-		ofn.lpstrDefExt		= DefExt();
-	
-		return ::GetSaveFileName( &ofn );
-	}
-
-	void	GetFileName( TString& str ){
-		str.Format(_T("%s\0"), m_szFileName);
-	}
 };
